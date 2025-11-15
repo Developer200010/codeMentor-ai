44ce4ade5025a1a3304e915744f375effb36b1af
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-11-15 10:21:47 +0530

    favicon is changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "../styles/globals.css";
-
+import Head from 'next/head';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.png" /> {/* Adjust href if your file name differs */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
